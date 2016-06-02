81b4a594cb7e9ad33863dcff209ef5f92117eedb
darthbatman <darthanakin44@gmail.com>
2016-06-02 16:45:10 -0400

    gets cpu coolers and fixed price issue

--- pcpartpicker.js
+++ pcpartpicker.js
@@ -1,6 +1,73 @@
 var request = require("request");
 var cheerio = require("cheerio");
 
+var getCoolers = function(cb){
+
+	var cooleroArray = [];
+	var nameArray = [];
+	var noiseArray = [];
+	var rpmArray = [];
+	var priceArray = [];
+
+	request("https://pcpartpicker.com/products/cpu-cooler/fetch/?mode=list&xslug=&search=", function(error, response, html1){
+
+		var numPages = parseInt(html1.toString().substring(html1.toString().lastIndexOf("page=") + 5).split("\\\"")[0]);
+
+		var fetch = function(q){	
+
+			if (q < numPages){	
+
+				request("https://pcpartpicker.com/products/cpu-cooler/fetch/?page=" + (q + 1) + "&mode=list&xslug=&search=", function(error, response, html){
+
+					var actualHTML = JSON.parse(response.body).result.html;
+
+					var $ = cheerio.load(actualHTML);
+
+					$('a').each(function(){
+						if ($(this).attr('href').indexOf('product') != -1){
+
+							if (actualHTML.substring(actualHTML.indexOf($(this).text()) + $(this).text().length + 30, actualHTML.indexOf($(this).text()) + $(this).text().length + 440).split("$")[1])
+							{
+								priceArray.push(parseFloat(actualHTML.substring(actualHTML.indexOf($(this).text()) + $(this).text().length + 30, actualHTML.indexOf($(this).text()) + $(this).text().length + 440).split("$")[1].split("<")[0]));
+							}
+							else {
+								priceArray.push("N/A");
+							}
+							nameArray.push($(this).text());
+							noiseArray.push(actualHTML.substring(actualHTML.indexOf($(this).text()) + $(this).text().length + 30, actualHTML.indexOf($(this).text()) + $(this).text().length + 300).split("right;\">")[2].split("<")[0].toLowerCase().replace("dba", ""));
+							rpmArray.push(actualHTML.substring(actualHTML.indexOf($(this).text()) + $(this).text().length + 10, actualHTML.indexOf($(this).text()) + $(this).text().length + 145).split(">")[1].split("<")[0].split(" RPM")[0]);
+						}
+					});
+
+					fetch(q + 1);
+
+					if (q == numPages - 1){
+						for (var i = 0; i < nameArray.length; i++){
+							cooleroArray.push({
+								name: nameArray[i],
+								price: priceArray[i],
+								noise: noiseArray[i],
+								rpm: rpmArray[i]
+							});
+
+							if (i == nameArray.length - 1){
+								cb (cooleroArray);
+							}
+						}
+					}
+
+				});
+
+			}
+			
+		}
+		
+		fetch(0);
+
+	});
+
+}
+
 var getCPUs = function(cb){
 
 	var cpuoArray = [];
@@ -8,6 +75,7 @@ var getCPUs = function(cb){
 	var speedArray = [];
 	var tdpArray = [];
 	var priceArray = [];
+	var coreArray = [];
 
 	request("https://pcpartpicker.com/parts/cpu/fetch/?mode=list&xslug=&search=", function(error, response, html1){
 
@@ -26,9 +94,9 @@ var getCPUs = function(cb){
 					$('a').each(function(){
 						if ($(this).attr('href').indexOf('product') != -1){
 							
-							if (actualHTML.substring(actualHTML.indexOf($(this).text()) + $(this).text().length - 320, actualHTML.indexOf($(this).text()) + $(this).text().length - 240).split("$")[1])
+							if (actualHTML.substring(actualHTML.indexOf($(this).text()) + $(this).text().length + 30, actualHTML.indexOf($(this).text()) + $(this).text().length + 440).split("$")[1])
 							{
-								priceArray.push(parseFloat(actualHTML.substring(actualHTML.indexOf($(this).text()) + $(this).text().length - 320, actualHTML.indexOf($(this).text()) + $(this).text().length - 240).split("$")[1].split("<")[0]));
+								priceArray.push(parseFloat(actualHTML.substring(actualHTML.indexOf($(this).text()) + $(this).text().length + 30, actualHTML.indexOf($(this).text()) + $(this).text().length + 440).split("$")[1].split("<")[0]));
 							}
 							else {
 								priceArray.push("N/A");
@@ -36,6 +104,7 @@ var getCPUs = function(cb){
 							nameArray.push($(this).text());
 							speedArray.push(parseFloat(actualHTML.substring(actualHTML.indexOf($(this).text()) + $(this).text().length + 30, actualHTML.indexOf($(this).text()) + $(this).text().length + 80).split(">")[1].split("<")[0].toLowerCase().split("ghz")[0]));
 							tdpArray.push(parseInt(actualHTML.substring(actualHTML.indexOf($(this).text()) + $(this).text().length + 100, actualHTML.indexOf($(this).text()) + $(this).text().length + 145).split(">")[1].split("<")[0].split("W")[0]));
+							coreArray.push(actualHTML.substring(actualHTML.indexOf($(this).text()) + $(this).text().length + 30, actualHTML.indexOf($(this).text()) + $(this).text().length + 480).split("center;\">")[1].split("<")[0]);
 						}
 					});
 
@@ -47,7 +116,8 @@ var getCPUs = function(cb){
 								name: nameArray[i],
 								price: priceArray[i],
 								speed: speedArray[i],
-								tdp: tdpArray[i]
+								tdp: tdpArray[i],
+								cores: coreArray[i]
 							});
 
 							if (i == nameArray.length - 1){
@@ -68,3 +138,4 @@ var getCPUs = function(cb){
 }
 
 module.exports.getCPUs = getCPUs;
+module.exports.getCoolers = getCoolers;
--- test.js
+++ test.js
@@ -3,3 +3,7 @@ var pcpartpicker = require("./pcpartpicker");
 pcpartpicker.getCPUs(function(CPUs){
 	console.log(CPUs);
 });
+
+pcpartpicker.getCoolers(function(coolers){
+	console.log(coolers);
+});
